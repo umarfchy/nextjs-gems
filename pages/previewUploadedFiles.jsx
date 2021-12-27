9cbea11731be900f3c3555aa792fe7b8a5b06399
umarfchy <umarfchy@gmail.com>
2021-12-27 08:24:30 +0600

    preview image, video and media when uploaded

---
+++ pages/previewUploadedFiles.jsx
@@ -0,0 +1,56 @@
+import { useState, useRef, useEffect } from "react";
+
+export default function SingleUploadedFile() {
+  const [file, setFile] = useState(null);
+  const [elementSrc, setElementSrc] = useState("");
+  const videoRef = useRef(null);
+  const imageRef = useRef(null);
+  const pdfRef = useRef(null);
+
+  useEffect(() => {
+    if (file) {
+      const src = URL.createObjectURL(new Blob([file], { type: file.type }));
+      setElementSrc(src);
+    }
+  }, [file]);
+
+  const defaultStyle = { width: "200px", height: "200px" };
+  const placeCenter = { display: "grid", placeItems: "center" };
+
+  return (
+    <div style={{ ...placeCenter, marginTop: "20px" }}>
+      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+      {
+        //previews the image
+        file && file.type.match("image") && (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            ref={imageRef}
+            src={elementSrc}
+            alt=""
+            style={{ ...defaultStyle, objectFit: "cover" }}
+          />
+        )
+      }
+      {
+        // previews the video
+        file && file.type.match("video") && (
+          <video
+            id="video-summary"
+            autoPlay
+            ref={videoRef}
+            controls
+            src={elementSrc}
+            style={defaultStyle}
+          />
+        )
+      }
+      {
+        // previews the pdf
+        file && file.type.match("application") && (
+          <embed ref={pdfRef} src={elementSrc} style={defaultStyle} />
+        )
+      }
+    </div>
+  );
+}
